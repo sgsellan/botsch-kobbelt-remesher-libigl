50c3bcd55493b43ba15042b95bea5785a6080ec3
sgsellan <sgsellan@gmail.com>
2020-10-30 11:52:11 -0400

    Closing issues #2 and #3: different file formats allowed and help is printed if no arguments given

--- remeshmesh.cpp
+++ remeshmesh.cpp
@@ -1,6 +1,6 @@
-#include <igl/readOBJ.h>
 #include "src/remesh_botsch.h"
-#include <igl/writeOBJ.h>
+#include <igl/read_triangle_mesh.h>
+#include <igl/write_triangle_mesh.h>
 
 using namespace std;
 
@@ -14,8 +14,21 @@ int main(int argc, const char* argv[]){
     int iterations = 10;
     double h = 0.05;
     out = "output.obj";
+    int argindex = 0;
+    if(argc>1){
 	    in = argv[1];
-	    int argindex = 1;
+	    argindex = 1;
+    }else{
+        std::cout<<R"(
+No input specified. To run in command line, issue
+
+./remeshmesh [input.ext] [output.ext] [-i num_iterations] [-h target_edge_length]
+
+The mesh in input.ext must be closed and manifold,
+and ext can be either of obj, ply, off, stl or mesh.
+)";
+        return argindex;
+    }
 	    while(argindex+1<argc){
 		    if(strncmp(argv[argindex+1],"-i",2)==0){
 			    iterations = atoi(argv[argindex+2]);
@@ -29,8 +42,8 @@ int main(int argc, const char* argv[]){
 		            argindex = argindex+1;
 		    }}
 	    }
-    igl::readOBJ(in,V,F);
+    igl::read_triangle_mesh(in,V,F);
     target = Eigen::VectorXd::Constant(V.rows(),h);
     remesh_botsch(V,F,target,iterations);
-    igl::writeOBJ(out,V,F);
+    igl::write_triangle_mesh(out,V,F);
 }
