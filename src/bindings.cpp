e9edf888ec44784e9293f3dd7ba0d777089ede1c
Baptiste Nicolet <baptiste.nicolet@epfl.ch>
2021-11-26 14:14:14 +0100

    Updated python bindings

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -7,12 +7,28 @@ npe_arg(f, dense_int)
 npe_arg(t, dense_double)
 npe_arg(i, int)
 npe_arg(ft, dense_int)
+npe_arg(project, bool)
 npe_begin_code()
     Eigen::MatrixXd V(v);
     Eigen::MatrixXi F(f);
     Eigen::VectorXd target(t);
     Eigen::VectorXi feature(ft);
-    remesh_botsch(V, F, target, i, feature);
+    remesh_botsch(V, F, target, i, feature, project);
+    return std::make_tuple(npe::move(V), npe::move(F));
+npe_end_code()
+
+npe_function(remesh_botsch)
+npe_arg(v, dense_double)
+npe_arg(f, dense_int)
+npe_arg(t, dense_double)
+npe_arg(i, int)
+npe_arg(ft, dense_int)
+npe_begin_code()
+    Eigen::MatrixXd V(v);
+    Eigen::MatrixXi F(f);
+    Eigen::VectorXd target(t);
+    Eigen::VectorXi feature(ft);
+    remesh_botsch(V, F, target, i, feature, false);
     return std::make_tuple(npe::move(V), npe::move(F));
 npe_end_code()
 
@@ -26,6 +42,20 @@ npe_begin_code()
     Eigen::MatrixXi F(f);
     Eigen::VectorXd t(target);
     remesh_botsch(V, F, t, i);
+    return std::make_tuple(npe::move(V), npe::move(F));
+npe_end_code()
+
+npe_function(remesh_botsch)
+npe_arg(v, dense_double)
+npe_arg(f, dense_int)
+npe_arg(target, dense_double)
+npe_arg(i, int)
+npe_arg(project, bool)
+npe_begin_code()
+    Eigen::MatrixXd V(v);
+    Eigen::MatrixXi F(f);
+    Eigen::VectorXd t(target);
+    remesh_botsch(V, F, t, i, project);
     return std::make_tuple(npe::move(V), npe::move(F));
 npe_end_code()
 
@@ -50,6 +80,19 @@ npe_begin_code()
     Eigen::MatrixXd V(v);
     Eigen::MatrixXi F(f);
     remesh_botsch(V, F, h, i);
+    return std::make_tuple(npe::move(V), npe::move(F));
+npe_end_code()
+
+npe_function(remesh_botsch)
+npe_arg(v, dense_double)
+npe_arg(f, dense_int)
+npe_arg(i, int)
+npe_arg(h, double)
+npe_arg(project, bool)
+npe_begin_code()
+    Eigen::MatrixXd V(v);
+    Eigen::MatrixXi F(f);
+    remesh_botsch(V, F, h, i, project);
     return std::make_tuple(npe::move(V), npe::move(F));
 npe_end_code()
 
